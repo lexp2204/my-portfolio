e554889c58a760a61a4e3c4cd0c29cffba3cdf56
Alexis Penn <lexp.0304@gmail.com>
2025-05-20 02:27:33 -0400

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Analytics } from "@vercel/analytics/react"
 import Nav from './Components/Nav'
 import Hero from './Components/Hero'
 import About from './Components/About'
